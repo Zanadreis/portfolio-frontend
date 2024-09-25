88e675c73f14e1088aff49d93578577e3bb6a8c8
Zanadreis <matheusolvic@hotmail.com>
2024-09-25 19:42:17 -0300

    fix: url link

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">{t('text')}</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://api.zanadreis.online" target="_blank">{t('api_btn')}</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://api.zanadreis.online/docs" target="_blank">{t('api_btn')}</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:ml-4" href="https://www.linkedin.com/in/matheus-z-53ba60140/" target="_blank">{t('contact_btn')}</a>
